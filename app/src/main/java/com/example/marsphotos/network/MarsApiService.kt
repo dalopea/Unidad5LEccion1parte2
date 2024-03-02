9d9829dc05cf18be8bb183ad4e3b75c51ffb9cca
dalopea <d.lopez.william@gmail.com>
2024-03-02 13:27:43 +0100

    Codelabs de internet acabado

--- app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -1,11 +1,16 @@
 package com.example.marsphotos.network
-import retrofit2.*
-import retrofit2.converter.scalars.ScalarsConverterFactory
+
+import com.example.marsphotos.model.MarsPhoto
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
 import retrofit2.http.GET
 
-private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com"
+private const val BASE_URL =
+    "https://android-kotlin-fun-mars-server.appspot.com"
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
     .baseUrl(BASE_URL)
     .build()
 
@@ -13,10 +18,12 @@ private val retrofit = Retrofit.Builder()
 
 interface MarsApiService {
     @GET("photos")
-    fun getPhotos():String
+    suspend fun getPhotos(): List<MarsPhoto>
 }
 
 object MarsApi {
+    val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
 
-    
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ plugins {
     id("com.android.application") version "8.1.3" apply false
     id("com.android.library") version "8.1.3" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
 }
 
 tasks.register("clean", Delete::class) {
